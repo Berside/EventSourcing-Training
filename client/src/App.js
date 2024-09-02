996b92f86543e38d7665665f34466869e5a0eb50
Berside <144826358+Berside@users.noreply.github.com>
2024-09-02 15:50:36 +0300

    init
    The basic implementation of Event Sourcing in the form of a real-time chat.
    
    A project for studying and training.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css'
+import EventSourcing from "./EventSourcing";
+
+function App() {
+  return (
+      <div>
+        <EventSourcing/>
+      </div>
+  )
+}
+export default App;
